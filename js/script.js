2f2c945d7d6b0f2d58bcbcdb2a723b235fdb7737
filauro1989 <filauro1989@gmail.com>
2021-11-23 16:25:23 +0100

    fix let

--- js/script.js
+++ js/script.js
@@ -5,28 +5,28 @@ for (let numero = 1; numero <= 100; numero++) {
 
     if ((numero % 5 == 0)&&(numero % 3 == 0)) {
 
-        div = document.createElement( 'div' );
+        let div = document.createElement( 'div' );
         div.innerHTML = 'FitzBuzz';
         container.appendChild(div); 
         div.className += "square red-bg";
 
     } else if (numero % 5 == 0) {
 
-        div = document.createElement( 'div' );
+        let div = document.createElement( 'div' );
         div.innerHTML = 'Buzz';
         container.appendChild(div);
         div.className += "square yellow-bg";
 
     } else if (numero % 3 == 0) {
 
-        div = document.createElement( 'div' );
+        let div = document.createElement( 'div' );
         div.innerHTML = 'Fitz';
         container.appendChild(div); 
         div.className += "square green-bg";
 
     } else {
 
-        div = document.createElement( 'div' );
+        let div = document.createElement( 'div' );
         div.innerHTML = numero;
         container.appendChild(div);
         div.className += "square blu-bg";

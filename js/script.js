932b54234fa137aeb9bacc0d790b1f7e437b50fc
filauro1989 <filauro1989@gmail.com>
2021-11-23 16:27:21 +0100

    cleaning

--- js/script.js
+++ js/script.js
@@ -3,30 +3,28 @@ const container = document.querySelector('.container');
 
 for (let numero = 1; numero <= 100; numero++) {
 
+    let div = document.createElement( 'div' );
+
     if ((numero % 5 == 0)&&(numero % 3 == 0)) {
 
-        let div = document.createElement( 'div' );
         div.innerHTML = 'FitzBuzz';
         container.appendChild(div); 
         div.className += "square red-bg";
 
     } else if (numero % 5 == 0) {
 
-        let div = document.createElement( 'div' );
         div.innerHTML = 'Buzz';
         container.appendChild(div);
         div.className += "square yellow-bg";
 
     } else if (numero % 3 == 0) {
 
-        let div = document.createElement( 'div' );
         div.innerHTML = 'Fitz';
         container.appendChild(div); 
         div.className += "square green-bg";
 
     } else {
 
-        let div = document.createElement( 'div' );
         div.innerHTML = numero;
         container.appendChild(div);
         div.className += "square blu-bg";
